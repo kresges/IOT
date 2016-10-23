2756b439eaf609cacd2d62a0da74e0b2e401228d
spencer <kresges@oregonstate.edu>
2016-10-23 10:27:15 -0700

    express app

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var routes = require('./routes/index')
+
+var app = express()
+
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs');
+
+app.use(express.static(__dirname +'/public'));
+
+app.use('/',routes);
+
+app.listen(process.env.PORT);
+
+
