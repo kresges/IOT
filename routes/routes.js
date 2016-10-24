6abd22d448fdb2ad60c82a653fa117f7bd5ab5ad
spencer <kresges@oregonstate.edu>
2016-10-23 22:51:10 -0700

    Working multi page express app

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/',function(req,res){
-  res.render('index');
+  res.render('index.ejs');
+});
+
+router.get('/newPage',function(req,res){
+  res.render('newPage.ejs')
 });
 
 module.exports = router;
